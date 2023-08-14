f90de13247c9f763599ea9ab7d960427c34be807
Jonathan Cain <jcainuk@gmail.com>
2023-08-14 16:38:40 +0100

    completes step 4

--- app.test.js
+++ app.test.js
@@ -53,3 +53,38 @@ describe("/api/topics", () => {
     });
   });
 });
+
+describe("/api/articles/:article_id", () => {
+  describe("GET an article", () => {
+    test("200: gets an article for an ID that exists", () => {
+      return request(app)
+        .get("/api/articles/1")
+        .then((response) => {
+          const { article } = response.body;
+          expect(article).toHaveProperty("article_id", 1);
+          expect(typeof article.article_id).toBe("number");
+          expect(typeof article.title).toBe("string");
+          expect(typeof article.topic).toBe("string");
+          expect(typeof article.author).toBe("string");
+          expect(typeof article.body).toBe("string");
+          expect(typeof article.created_at).toBe("string");
+        });
+    });
+    test("400: responds with error when invalid id is input", () => {
+      return request(app)
+        .get("/api/articles/apple")
+        .expect(400)
+        .then((response) => {
+          expect(response.body.msg).toBe("Bad request");
+        });
+    });
+    test("404: responds with an error when valid input but dont have that article", () => {
+      return request(app)
+        .get("/api/articles/9000")
+        .expect(404)
+        .then((response) => {
+          expect(response.body.msg).toBe("Article does not exist");
+        });
+    });
+  });
+});
