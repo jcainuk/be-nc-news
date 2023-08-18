23a89a993b836e8ec9ccd582358ea88d16dd1516
Jonathan Cain <jcainuk@gmail.com>
2023-08-18 12:45:20 +0100

    debugging listen and connection files

--- listen.js
+++ listen.js
@@ -1,7 +1,4 @@
-const express = require("express");
-const app = express();
-
 const app = require("./app.js");
 const { PORT = 9090 } = process.env;
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
